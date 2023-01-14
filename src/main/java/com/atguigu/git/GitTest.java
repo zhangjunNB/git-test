d28484b70187442550572708a16647d806843923
zhangjun <759329154@qq.com>
2023-01-14 23:24:20 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author zjstart
+ * @description <TODO description class purpose>
+ * @create 2023-01-14 22:37
+ */
+public class GitTest {
+    public static void main(String args[]){
+        System.out.println("hello git!");
+    }
+}
